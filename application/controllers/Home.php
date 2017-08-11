871859d88e22cff47d6e592ecba96c8a6e54cdd7
Emese <jakabemese92@gmail.com>
2017-08-11 13:50:21 +0200

    controllers

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,11 +3,28 @@ class Home extends CI_Controller {
     
     public function index()
     {
-        $data['user'] = "asdasd"; //ucfirst($page); // Capitalize the first letter
         
-        $this->load->view('templates/header', $data);
-        $this->load->view('home/index.php', $data);
-        $this->load->view('templates/footer', $data);
+        if($this->session->userdata('logged_in'))
+        {
+            $session_data = $this->session->userdata('logged_in');
+            $data['name'] = $session_data['name'];
+           
+            $this->load->view('templates/header', $data);
+            $this->load->view('home/index.php', $data);
+            $this->load->view('templates/footer', $data);
+        }
+        else
+        {
+            //If no session, redirect to login page
+            redirect('login', 'refresh');
+        }
+    }
+    
+    function logout()
+    {
+        $this->session->unset_userdata('logged_in');
+        session_destroy();
+        redirect('home', 'refresh');
     }
     
     public function view($page = 'home')
