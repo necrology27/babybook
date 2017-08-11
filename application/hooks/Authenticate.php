9d04f2629a2e04b203b106fa06ac23d34b666a58
Akos Orban <orban.akos@yahoo.com>
2017-08-11 10:16:23 +0300

    gitignore database.php

--- application/hooks/Authenticate.php
+++ application/hooks/Authenticate.php
@@ -5,6 +5,7 @@ class Authenticate{
     public function __construct() {
         $this->CI = & get_instance();
     }
+    
     public function check_user_login(){
         if(!$this->CI->session->is_logged_in){
             redirect('main/restricted');
