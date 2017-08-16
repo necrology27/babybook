0b0c1cec3cace6c3fb320678707d1812b5d066b2
Akos Orban <orban.akos@yahoo.com>
2017-08-16 13:03:12 +0300

    user_model + Home controller

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -20,6 +20,49 @@ class Home extends CI_Controller {
         }
     }
     
+    function update()
+    {
+        // set validation rules
+        $this->form_validation->set_rules('name', 'Name', 'trim|required|callback_alpha_dash_space|min_length[3]|max_length[30]|xss_clean');
+        $this->form_validation->set_rules('newpassword', 'New Password', 'trim|sha1');
+        $this->form_validation->set_rules('cpassword', 'Confirm Password', 'trim|matches[newpassword]|sha1');
+        $this->form_validation->set_rules('gender', 'Gender', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[users.email]');
+        $this->form_validation->set_rules('birthday', 'Birthday', 'trim|required|callback_valid_date');
+        $this->form_validation->set_rules('measurement', 'Measurement', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required|sha1');
+        
+        // validate form input
+        if ($this->form_validation->run() == FALSE) {
+            // fails
+            $this->load->view('home/edit_self');
+        } else {
+            // insert the user registration details into database
+            $data = array(
+                'name' => $this->input->post('name'),
+                'password' => $this->input->post('newpassword'),
+                'gender' => $this->input->post('gender'),
+                'email' => $this->input->post('email'),
+                'birthday' => $this->input->post('birthday'),
+                'role' => 2,
+                'language' => 1,
+                'measurement' => $this->input->post('measurement')
+            );
+            
+            // insert form data into database
+            if ($this->user_model->updateUser($data)) {
+                
+                // successfully sent mail
+                $this->session->set_flashdata('msg', '<div class="alert alert-success text-center">You are Successfully Registered!</div>');
+                redirect('user/register');
+            } else {
+                // error
+                $this->session->set_flashdata('msg', '<div class="alert alert-danger text-center">Oops! Error. Can\'t insert data.  Please try again later!!!</div>');
+                redirect('user/register');
+            }
+        }
+    }
+    
     function logout()
     {
         $this->session->unset_userdata('logged_in');
--- application/models/user_model.php
+++ application/models/user_model.php
@@ -17,6 +17,27 @@ class user_model extends CI_Model
 	    return $this->db->insert('children', $data);
 	}
 	
+	function get_user_id($data){
+	    $email[] = $data['email'];
+	    $this->db->select('id');
+	    $this->db->from('users');
+	    $this->db->where('email', $email);
+	    $query = $this->db->get();
+	    $result = $query->result();
+	    return $result;
+	}
+	
+	function update_user_by_id($id,$data){
+	    $this->db->where('id', $id);
+	    return $this->db->update('users', $data);
+	}
+	
+	function updateUser($data)
+	{
+	    $id = $this->get_user_id($data);
+	    return $this->update_user_by_id($id, $data);
+	}
+	
 	function changePassword($data, $pwd)
 	{
 	    $sha_password = sha1($pwd);
