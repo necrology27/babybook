7f97866f2a25d3e69ec1ddbd6f3a638ec97fd5dc
Emese <jakabemese92@gmail.com>
2017-08-17 09:40:52 +0200

    :)

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -2,6 +2,7 @@
 
 class user extends CI_Controller
 {
+    
 
     public function __construct()
     {
@@ -117,20 +118,45 @@ class user extends CI_Controller
     }
     function add_child()
     {
+      
+        $session_data = $this->session->userdata('logged_in');
+        $userId = $session_data['id'];
+        
         // set validation rules
         $this->form_validation->set_rules('name', 'Name', 'trim|required|callback_alpha_dash_space|min_length[3]|max_length[30]|xss_clean');
         $this->form_validation->set_rules('birthday', 'Birthday', 'trim|required|callback_valid_date');
         
+       
+        
         // validate form input
         if ($this->form_validation->run() == FALSE) {
             // fails
-            $this->load->view('add_child');
+            $this->load->view('add_child', $data);
         } else {
+            
+            if($this->input->post('is_parent') == NULL)
+            {
+                $is_par=0;
+            }
+            else
+                $is_par=1;
+                
             // insert the user registration details into database
             $data = array(
                 'name' => $this->input->post('name'),
-                'password' => $this->input->post('password'),
-          
+                'birthday' => $this->input->post('birthday'),
+                'gender' => $this->input->post('gender'),
+                'birth_weight' => $this->input->post('birth_weight'),
+                'birth_length' => $this->input->post('birth_length'),
+                'apgar_score' => $this->input->post('apgar_score'),
+                'genetical_disorders' => $this->input->post('genetical_disorders'),
+                'other_disorders' => $this->input->post('other_disorders'),
+                //                 image
+                
+                'is_parent' =>  $is_par,
+                'other_disorders' => $this->input->post('other_disorders'),
+                'userId' => $userId,
+
             );
             
             // insert form data into database
@@ -138,11 +164,11 @@ class user extends CI_Controller
                 
                 // successfully sent mail
                 $this->session->set_flashdata('msg', '<div class="alert alert-success text-center">You are Successfully ADD A CHILD!</div>');
-                redirect('user/register');
+                redirect('user/add_child');
             } else {
                 // error
                 $this->session->set_flashdata('msg', '<div class="alert alert-danger text-center">Oops! Error. Can\'t ADD CHILD.  Please try again later!!!</div>');
-                redirect('user/register');
+                redirect('user/add_child');
             }
         }
     }
--- application/views/add_child.php
+++ application/views/add_child.php
@@ -34,12 +34,12 @@ $( function() {
 						<h4>Add a child</h4>
 					</div>
 					<div class="panel-body">
-                <?php $attributes = array("name" => "registrationform");
-                echo form_open("user/register", $attributes); ?>
-                		
+                <?php $attributes = array("name" => "addchildform");
+                echo form_open("user/add_child", $attributes); ?>
+                		<?php echo $measurement?>
                 		<div class="form-group">
 							<label for="name">Name</label> <input class="form-control"
-								name="name" placeholder="Your Name" type="text"
+								name="name" placeholder="Child's name" type="text"
 								value="<?php echo set_value('name'); ?>" /> <span
 								class="text-danger"><?php echo form_error('name'); ?></span>
 						</div>
@@ -74,7 +74,7 @@ $( function() {
 						<div class="form-group">
 							<label for="birth_length">Birth length </label> <br>
 							<input class="form-control2" name="birth_length" type="text" value="<?php echo set_value('birth_length'); ?>" /> 
-								<span class= "measurement">m</span>
+								<span class= "measurement">cm</span>
 								<span class="text-danger">
 	
 								<?php echo form_error('birth_weight'); ?>
@@ -109,24 +109,25 @@ $( function() {
 
 						
 						<div class="form-group">
-							<label for="email">Add default image</label> 
+							<label for="image">Add default image</label> 
 							 <input type="file" name="image" accept="image/*"> 
 								<span class="text-danger">
-								<?php echo form_error('email'); ?>
+								<?php echo form_error('image'); ?>
 								</span>
 						</div>
 						
 							<div class="form-group">
 						<!--	<label for="birthday">Birthday</label> -->
-							<input type="checkbox" name="is_parent" value="<?php echo set_value('is_parent'); ?>"> My child.<br>
+							<input type="checkbox" name="is_parent" value="is_parent"> My child.<br>
 							<?php echo form_error('is_parent'); ?>
 							</span>
 						</div>
 						
 
 						<div class="form-group">
-							<button name="submit" type="submit" class="btn btn-default">Signup</button>
-							<button name="cancel" type="reset" class="btn btn-default">Cancel</button>
+							<button name="submit" type="submit" class="btn btn-default">Add</button>
+							<button name="cancel" type="reset" class="btn btn-default" onclick="location.href='<?php echo base_url();?>home'">Cancel</button>
+							
 						</div>
 					</form>
                 <?php echo $this->session->flashdata('msg'); ?>
