acf4020f0ab153b4e77ddc1e381e595f503bbecf
Akos Orban <orban.akos@yahoo.com>
2017-08-18 15:29:19 +0300

    css szebb, popup javitva

--- resources/main.js
+++ resources/main.js
@@ -3,7 +3,8 @@ function popup_function(id) {
 	var popups = document.getElementsByClassName("show");
 	var i;
 	for (i = 0; i < popups.length; i++) {
-	    popups[i].classList.toggle("show");
+		if (popups[i].getAttribute('id') != id)
+			popups[i].classList.toggle("show");
 	} 
     var popup = document.getElementById(id);
     popup.classList.toggle("show");
